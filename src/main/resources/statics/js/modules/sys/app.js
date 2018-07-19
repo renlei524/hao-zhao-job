b50cf8f51f8c62db8120d27dd25ed1b64a55ec39
renlei <renlei@scxxwb.com>
2018-07-19 18:00:59 +0800

    app删除后弹出框提示

--- src/main/resources/statics/js/modules/sys/app.js
+++ src/main/resources/statics/js/modules/sys/app.js
@@ -191,7 +191,7 @@ var vm = new Vue({
                     data: JSON.stringify(ids),
                     success: function(r){
                         if(r.code == 0){
-                            alert('','操作成功', function(index){
+                            alert('操作成功', function(index){
                                 $("#jqGrid").trigger("reloadGrid");
                             });
                         }else{
