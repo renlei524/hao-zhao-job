31e66a856d6225b708abd3516c0496d7e405c31c
yuhuan <yuhuan@scxxwb.com>
2018-07-23 11:19:21 +0800

    微易客接口文档

--- src/main/java/com/scxxwb/net/admin/modules/operation/controller/TVYiCooCategoryController.java
+++ src/main/java/com/scxxwb/net/admin/modules/operation/controller/TVYiCooCategoryController.java
@@ -5,20 +5,22 @@ import com.scxxwb.net.admin.common.utils.R;
 import com.scxxwb.net.admin.modules.operation.entity.TVYiCooCategoryEntity;
 import com.scxxwb.net.admin.modules.operation.service.TVYiCooCategoryService;
 import com.scxxwb.net.admin.modules.sys.controller.AbstractController;
+import io.swagger.annotations.*;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Map;
 
+import static com.qcloud.cos.http.RequestHeaderValue.Method.POST;
+
 
 /**
- * 经营类目
+ * 微易客经营类目
  *
  */
+@Api(value = "operation/tvyicoocategory", tags = "微易客经营类目")
 @RestController
 @RequestMapping("operation/tvyicoocategory")
 public class TVYiCooCategoryController extends AbstractController {
@@ -30,21 +32,29 @@ public class TVYiCooCategoryController extends AbstractController {
      */
     @RequestMapping("/list")
     @RequiresPermissions("operation:tvyicoocategory:list")
-    public R list(@RequestParam Map<String, Object> params){
+    @ApiOperation(value = "列表", httpMethod = POST)
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "pageNum", required = true, value = "当前页码"),
+//    })
+    @ApiImplicitParam(name = "params", value = "查询信息", required = true, dataType = "json")
+    public R list(@ApiIgnore() @RequestParam Map<String, Object> params){
         PageUtils page = tViYiCooCategoryService.queryPage(params);
 
         return R.ok().put("page", page);
     }
-
 
     /**
      * 信息
      */
     @RequestMapping("/info/{id}")
     @RequiresPermissions("operation:tvyicoocategory:info")
-    public R info(@PathVariable("id") String id){
+    @ApiOperation(value = "根据id查询微易客经营类目信息", httpMethod = POST)
+        public R info(
+            @ApiParam(value = "经营类目id", required=true) @RequestParam Integer id
+        ){
          TVYiCooCategoryEntity tvYiCooCategoryEntity = tViYiCooCategoryService.selectById(id);
 
         return R.ok().put("tvYiCooCategoryEntity", tvYiCooCategoryEntity);
     }
+
 }
